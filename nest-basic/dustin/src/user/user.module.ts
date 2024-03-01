25d237194ea976028811fd6470f500fa8d75c5b4
GiWoonHwang <rlaejrqo465@naver.com>
2024-03-01 23:20:59 +0900

    5.모듈설계

--- nest-basic/dustin/src/user/user.module.ts
+++ nest-basic/dustin/src/user/user.module.ts
@@ -1,11 +1,11 @@
-import { EmailService } from 'src/email/email.service';
 import { UserController } from './user.controllers';
 import { UserService } from './user.service';
 import { Module } from '@nestjs/common';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [],
+  imports: [EmailModule],
   controllers: [UserController],
-  providers: [UserService, EmailService],
+  providers: [UserService],
 })
 export class UserModule {}
