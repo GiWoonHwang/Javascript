4eb1c9e1ff730e46b02228fce616b72c9c3460a3
GiWoonHwang <rlaejrqo465@naver.com>
2024-03-04 13:50:22 +0900

    클린 아키텍쳐 변환 중간저장

---
+++ nest-basic/dustin/src/user/domain/user.ts
@@ -0,0 +1,23 @@
+// 유저 도메인의 aggregate
+
+export class User {
+  constructor(
+    private id: string,
+    private name: string,
+    private email: string,
+    private password: string,
+    private signupVerifyToken: string,
+  ) {}
+
+  getId(): Readonly<string> {
+    return this.id;
+  }
+
+  getName(): Readonly<string> {
+    return this.name;
+  }
+
+  getEmail(): Readonly<string> {
+    return this.email;
+  }
+}
